9b9260eed88daee47bf976b201f5382eb97cd4a9
dossancto <dossantossilvalucas963@gmail.com>
2026-05-10 19:07:51 -0300

    init

--- src/main.zig
+++ src/main.zig
@@ -3,69 +3,98 @@ const Io = std.Io;
 
 const cuxh = @import("cuxh");
 
+// pub fn main(init: std.process.Init) !void {
+//     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+//
+//     const arena: std.mem.Allocator = init.arena.allocator();
+//
+//     const args = try init.minimal.args.toSlice(arena);
+//
+//     for (args) |arg| {
+//         const a = .{arg};
+//
+//         std.log.info("arg: {s}", a);
+//     }
+//
+//     const io = init.io;
+//
+//     var stdout_buffer: [1024]u8 = undefined;
+//     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
+//     const stdout_writer = &stdout_file_writer.interface;
+//
+//     try cuxh.printAnotherMessage(stdout_writer);
+//
+//     try stdout_writer.flush(); // Don't forget to flush!
+// }
+//
+
+const CurlMetadata = struct {
+    method: []const u8,
+    url: []const u8,
+    Headers: std.ArrayList([][]const u8),
+};
+
 pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("Hello World", .{});
 
-    // This is appropriate for anything that lives as long as the process.
-    const arena: std.mem.Allocator = init.arena.allocator();
+    const arena = init.arena.allocator();
 
-    // Accessing command line arguments:
     const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
+
+    const curl_string = args[1];
+
+    std.debug.print("curl_string: {s}\n", .{curl_string});
+}
+
+fn parse_curl(curl_string: []const u8) CurlMetadata {
+    _ = std.mem.splitScalar(u8, curl_string, ' ');
+
+    const metadata = CurlMetadata{
+        .method = "",
+        .url = "",
+        .Headers = std.ArrayList([][]const u8).init(std.heap.page_allocator),
+    };
+
+    return metadata;
+}
+
+fn valid_curl_string(curl_string: []const u8) bool {
+    return std.mem.startsWith(u8, curl_string, "curl");
+}
+
+test "valid_curl_string" {
+    try std.testing.expect(valid_curl_string("curl -X GET https://example.com"));
+
+    try std.testing.expect(valid_curl_string("some random text") == false);
+
+    try std.testing.expect(valid_curl_string("wget https://example.com") == false);
+}
+
+test "can split string" {
+    const text = "curl test teste";
+
+    var parts = std.mem.splitScalar(u8, text, ' ');
+
+    var count: u8 = 0;
+
+    while (parts.next()) |a| {
+        if (count == 0) {
+            count += 1;
+            continue;
+        }
+        count += 1;
+        _ = a;
     }
 
-    // In order to do I/O operations need an `Io` instance.
-    const io = init.io;
-
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
-
-    try cuxh.printAnotherMessage(stdout_writer);
-
-    try stdout_writer.flush(); // Don't forget to flush!
+    try std.testing.expect(count == 3);
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
-
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
-            );
-        },
-    };
-}
+// test "parse_curl" {
+//     const curl_string = "curl -X POST \"https://httpbin.org/post\" -H  \"accept: application/json\" --data-raw '{\"property1\": \"1\"}' -H 'Authorization: Bearer 123'";
+//
+//     _ = parse_curl(curl_string);
+//
+//     // const is_post = std.mem.eql(u8, metadata.method, "POST");
+//     //
+//     // try std.testing.expect(is_post);
+// }
