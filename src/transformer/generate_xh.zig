7823fd881b0eda385e4926f9ef3609c9151bc65e
dossancto <dossantossilvalucas963@gmail.com>
2026-05-13 22:49:46 -0300

    use correct protocol on xh

--- src/transformer/generate_xh.zig
+++ src/transformer/generate_xh.zig
@@ -4,7 +4,15 @@ const curl_handler = @import("../curl/curl_handler.zig");
 pub fn curl_to_xh(curl: curl_handler.CurlMetadata, allocator: std.mem.Allocator) ![]const u8 {
     var builder = std.ArrayList(u8).empty;
 
-    try builder.appendSlice(allocator, "xh");
+    if (curl.url.is_http_protocol() == false) {
+        return error.OnlyHttpProtocolIsSupported;
+    }
+
+    if (curl.url.is_secure_protocol() == true) {
+        try builder.appendSlice(allocator, "xhs");
+    } else {
+        try builder.appendSlice(allocator, "xh");
+    }
     try builder.append(allocator, ' ');
 
     try builder.appendSlice(allocator, curl.method);
@@ -79,7 +87,7 @@ test "Generate xh command" {
     const metadata = try curl_handler.CurlMetadata.parse_curl(curl_string, allocator);
     const xh_command = try curl_to_xh(metadata, allocator);
 
-    const expected_xh_command = "xh POST https://httpbin.org/post --raw '{\"property1\": \"1\"}' 'accept':'application/json' --bearer '123123'";
+    const expected_xh_command = "xhs POST https://httpbin.org/post --raw '{\"property1\": \"1\"}' 'accept':'application/json' --bearer '123'";
 
     try std.testing.expect(std.mem.eql(u8, xh_command, expected_xh_command));
 }
@@ -93,7 +101,7 @@ test "Generate xh command on localhost" {
     const metadata = try curl_handler.CurlMetadata.parse_curl(curl_string, allocator);
     const xh_command = try curl_to_xh(metadata, allocator);
 
-    const expected_xh_command = "xh POST :5000/users --raw '{\"property1\": \"1\"}' 'accept':'application/json' --bearer '123123'";
+    const expected_xh_command = "xh POST :5000/users --raw '{\"property1\": \"1\"}' 'accept':'application/json' --bearer '123'";
 
     try std.testing.expect(std.mem.eql(u8, xh_command, expected_xh_command));
 }
